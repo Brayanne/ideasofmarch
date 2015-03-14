ac86b6525a0bdd6ad9341299de5f0450531cd7c4
reye6671 <brayannedreyes@student.hartnell.edu>
2015-03-14 15:09:59 -0700

    Connection between pages

--- app/src/main/java/com/ideas/saymtfmtfmtf/ideasofmarch/Map.java
+++ app/src/main/java/com/ideas/saymtfmtfmtf/ideasofmarch/Map.java
@@ -4,43 +4,21 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.content.Intent;
 
 
-public class home extends ActionBarActivity {
+public class Map extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-
-        Button Home1 = (Button) findViewById(R.id.Home1);
-        Home1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), MainActivity.class);
-                startActivityForResult(intent, 0);
-            }
-        });
-
-        Button pollingMap = (Button) findViewById(R.id.pollingMap);
-        pollingMap.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), Map.class);
-                startActivityForResult(intent, 0);
-            }
-        });
-
+        setContentView(R.layout.activity_map);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
+        getMenuInflater().inflate(R.menu.menu_map, menu);
         return true;
     }
 
