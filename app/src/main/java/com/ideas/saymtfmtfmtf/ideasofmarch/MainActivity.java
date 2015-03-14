3add8390eb42549f86f19ea89338d596b62c13df
fent4647 <mfenton@csumb.edu>
2015-03-14 08:40:17 -0700

    Deleted XML Login

--- app/src/main/java/com/ideas/saymtfmtfmtf/ideasofmarch/MainActivity.java
+++ app/src/main/java/com/ideas/saymtfmtfmtf/ideasofmarch/MainActivity.java
@@ -25,7 +25,6 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        TextView hi = (TextView) findViewById(R.id.textView);
 
 
         return true;
