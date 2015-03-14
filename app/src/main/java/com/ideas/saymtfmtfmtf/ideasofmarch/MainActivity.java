c4818698d4162e1eda1a8e1b0cac679151a5edd3
Maria Rivera <marivera@csumb.edu>
2015-03-13 20:30:03 -0700

    ADded Text

--- app/src/main/java/com/ideas/saymtfmtfmtf/ideasofmarch/MainActivity.java
+++ app/src/main/java/com/ideas/saymtfmtfmtf/ideasofmarch/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -19,6 +20,8 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        TextView hi = (TextView) findViewById(R.id.textView);
+
 
         return true;
     }
