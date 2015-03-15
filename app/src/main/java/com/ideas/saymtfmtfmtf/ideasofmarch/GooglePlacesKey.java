c4de74b0ce849305ad64a46c459235be02fd9484
fent4647 <mfenton@csumb.edu>
2015-03-14 18:02:07 -0700

    Add To Manifest

--- app/src/main/java/com/ideas/saymtfmtfmtf/ideasofmarch/GooglePlacesKey.java
+++ app/src/main/java/com/ideas/saymtfmtfmtf/ideasofmarch/GooglePlacesKey.java
@@ -4,5 +4,5 @@ package com.ideas.saymtfmtfmtf.ideasofmarch;
  * Created by mgr1010 on 3/14/15.
  */
 public class GooglePlacesKey {
-    public static final String API_KEY = "";
+    public static final String API_KEY = "AIzaSyAAwDWd1ilKdQCt7a-AtXoIiguRDe456zg";
 }
--- app/src/main/java/com/ideas/saymtfmtfmtf/ideasofmarch/home.java
+++ app/src/main/java/com/ideas/saymtfmtfmtf/ideasofmarch/home.java
@@ -42,7 +42,6 @@ public class home extends ActionBarActivity {
                 startActivityForResult(intent, 0);
             }
         });
-
     }
 
 
